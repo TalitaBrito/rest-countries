e30ede1146930cdae76bd995d9770e2f0ba10bb3
Talita Brito <talita_brito96@hotmail.com>
2023-01-18 13:22:52 -0300

    add comp header

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import styles from "./Header.module.css";
-import Button from "../atoms/Button";
+import Button from "../../atoms/Button";
 
 function Header({ changeTheme }) {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Where in the word?</h1>
+      <h1 className={styles.title}>Where in the world?</h1>
       <Button onClick={changeTheme} icon={<ion-icon name="moon-outline" />}>
         Theme teste
       </Button>
