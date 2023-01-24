fe0c473cc65e9dec8a48b149724dfbd3d64f817f
Talita Brito <talita_brito96@hotmail.com>
2023-01-24 18:40:08 -0300

    create CardCountry component

--- src/components/molecules/CardCountry/CardCountry.jsx
+++ src/components/molecules/CardCountry/CardCountry.jsx
@@ -1,33 +1,41 @@
 import PropTypes from "prop-types";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link } from "react-router-dom";
 import styles from "./CardCountry.module.css";
 
-function CardCountry({ flagImg, country, population, region, capital }) {
+function CardCountry({ flagImg, country, population, region, capital, code }) {
+  // const { countryName } = useParams();
+
   return (
     <section className={styles.container}>
-      <img
-        className={styles.flag}
-        src={flagImg}
-        alt={`Bandeira do país: ${country}`}
-      />
+      <Link to={`/details-country/${code}`} className={styles.link}>
+        <img
+          className={styles.flag}
+          src={flagImg}
+          alt={`Bandeira do país: ${country}`}
+        />
 
-      <div className={styles.infoContainer}>
-        <h3 className={styles.title}>{country}</h3>
+        <div className={styles.infoContainer}>
+          <h3 className={styles.title}>{country}</h3>
 
-        <div>
-          <strong>Population: </strong>
-          <span>{population}</span>
+          <div>
+            <strong>Population: </strong>
+            <span>{population}</span>
+          </div>
+
+          <div>
+            <strong>Region: </strong>
+            <span>{region}</span>
+          </div>
+
+          {capital && (
+            <div>
+              <strong>Capital: </strong>
+              <span>{capital}</span>
+            </div>
+          )}
         </div>
-
-        <div>
-          <strong>Region: </strong>
-          <span>{region}</span>
-        </div>
-
-        <div>
-          <strong>Capital: </strong>
-          <span>{capital}</span>
-        </div>
-      </div>
+      </Link>
     </section>
   );
 }
@@ -37,7 +45,12 @@ CardCountry.propTypes = {
   country: PropTypes.string.isRequired,
   population: PropTypes.string.isRequired,
   region: PropTypes.string.isRequired,
-  capital: PropTypes.string.isRequired,
+  capital: PropTypes.string,
+  code: PropTypes.string.isRequired,
+};
+
+CardCountry.defaultProps = {
+  capital: "",
 };
 
 export default CardCountry;
