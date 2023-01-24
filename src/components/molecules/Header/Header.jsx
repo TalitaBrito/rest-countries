649ca19a617e4e439f3b1a890690139108508a25
Talita Brito <talita_brito96@hotmail.com>
2023-01-24 18:40:57 -0300

    adjust import

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import styles from "./Header.module.css";
-import Button from "../../atoms/Button";
+import Button from "../../atoms/Button/Button";
 
 function Header({ changeTheme }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Where in the world?</h1>
+
       <Button onClick={changeTheme} icon={<ion-icon name="moon-outline" />}>
         Theme teste
       </Button>
